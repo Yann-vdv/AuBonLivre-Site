da75b3bcc99c86a08ce15761900920f3bfd7fdf9
Naplucia <fldebruyck@gmail.com>
2023-06-03 18:44:11 +0200

    add home screen
    ajout de home avec la liste des livres
    ajout du css de home
    ajout des images "oops"(pour placeholder) et sac de cource pour home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { useAuth } from '../context/Auth';
+import BAG from "../images/sac-de-courses.png"
+import '../styles/home.css'
+import { useState } from 'react';
+import {useNavigate } from 'react-router-dom';
 
 function Home() {
 
@@ -10,13 +14,44 @@ function Home() {
     React.useEffect(() => {
         console.log("i'm home")
     })
+    
+  const [cardToShow, setCardToShow] = React.useState(8);
 
   return (
-    <div className="Home">
-        Home
-        <button onClick={() => logout()}>deco</button>
-    </div>
+    <div className="Home"> 
+    {/* <h1>Nombre de joueurs : {Joueur_variable.length}</h1> */}
+      <h2>Nos dernières nouveautés</h2>
+      
+      <div className='wrapper_ensemble_cartes'>
+        {[...Array(cardToShow)].map(
+          (value, index)=>(
+            <Card id={index+1} key={index}/>
+          )
+        )}
+      </div>
+      {/* <Ajout/> */}
+  </div>
   );
+}
+
+const Card = ({id}: {id:number})=>{
+  const add=()=>{
+
+  }
+  return (<div className="wrapper-carte">
+                    <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhPC6Q_Kpxa_CJcPij6rjAUn0vu6FuHxLHMb8PLCM&s"} className="book-image" alt=""/>
+                   {/* <h3 className="Attribut_Nom">{variable.name}</h3> */}
+                    <h5 className="book-title">titre</h5>
+                <div className="card-detail">
+                    <p className="card-price">price</p>
+                    <div onClick={add} className='home-add-button'>
+                      <p>Add</p>
+                      <img src={BAG}></img>
+                      
+                    </div>
+                </div>
+            </div>
+            );
 }
 
 export default Home;
