fc3a346011989d9228626ee75de399b178636b9c
Yann-vdv <61906860+Yann-vdv@users.noreply.github.com>
2023-06-08 00:32:35 +0200

    sideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -8,7 +8,7 @@ function SideBar() {
     ])
 
     return (
-        <nav className="bg-white w-60 h-screen border-t border-t-gray-700' border-r border-gray-400 border-black">
+        <nav className="bg-white w-60 h-full border-t border-t-gray-700' border-r border-gray-400 border-black">
             {categorys.map((category) => 
                 <div className='w-full border-b border-gray-700' style={{backgroundColor:'#D9D9D9'}} key={category.id}>
                     <h3 className='text-3xl text-center p-4 cursor-pointer hover:font-medium hover:text-sky-400'>{category.name}</h3>
